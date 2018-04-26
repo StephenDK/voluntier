6b2b080181490582a622791abfe6a696e6b6a5a3
StephenDK <stephendklein08@gmail.com>
2018-04-26 09:03:54 -0700

    production server setup

--- server.js
+++ server.js
@@ -53,6 +53,18 @@ require('./routes/authRoutes')(app);
 // require routes for db querys
 require('./routes/apiRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve production assets
+  // like our main.js file, or main.css file
+  app.use(express.static('client/build'));
+  // Express will serve up the index.html file
+  // if it doesent recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 
 // Start the API server
 app.listen(PORT, function() {
